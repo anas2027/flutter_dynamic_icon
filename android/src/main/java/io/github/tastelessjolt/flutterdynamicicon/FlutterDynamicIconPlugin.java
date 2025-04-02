754bc8741f79c74394fdae30563b247e52d0ca51
Anas <anasalsamman2027@gmail.com>
2025-04-02 04:38:44 -0700

    Update FlutterDynamicIconPlugin.java
    fix flutter dynaimc icon to work with latest flutter sdk version

--- android/src/main/java/io/github/tastelessjolt/flutterdynamicicon/FlutterDynamicIconPlugin.java
+++ android/src/main/java/io/github/tastelessjolt/flutterdynamicicon/FlutterDynamicIconPlugin.java
@@ -5,16 +5,9 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
 
-/** FlutterDynamicIconPlugin */
 public class FlutterDynamicIconPlugin implements FlutterPlugin {
   private static final String CHANNEL_NAME = "flutter_dynamic_icon";
   private MethodChannel channel;
-
-  @SuppressWarnings("deprecation")
-  public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
-    final FlutterDynamicIconPlugin plugin = new FlutterDynamicIconPlugin();
-    plugin.setupChannel(registrar.messenger(), registrar.context());
-  }
 
   @Override
   public void onAttachedToEngine(FlutterPlugin.FlutterPluginBinding binding) {
@@ -28,12 +21,13 @@ public class FlutterDynamicIconPlugin implements FlutterPlugin {
 
   private void setupChannel(BinaryMessenger messenger, Context context) {
     channel = new MethodChannel(messenger, CHANNEL_NAME);
-    MethodCallHandlerImpl handler = new MethodCallHandlerImpl();
-    channel.setMethodCallHandler(handler);
+    channel.setMethodCallHandler((call, result) -> {
+      result.notImplemented(); // Add Android-specific logic here if needed
+    });
   }
 
   private void teardownChannel() {
     channel.setMethodCallHandler(null);
     channel = null;
-  }  
+  }
 }
